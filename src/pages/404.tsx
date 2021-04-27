4f214b17153bdccfc05f10c3a2e30b50e76b6ad9
unknown <lcanavesio@gmail.com>
2021-04-27 20:10:01 -0300

    update package & add typescript

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,8 +4,8 @@ import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <>
-    <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
+    <SEO title="404: error inesperado" />
+    <h1>404: error inesperado</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </>
 )
---
+++ src/utils/typography.js
@@ -0,0 +1,10 @@
+import Typography from "typography"
+
+const typography = new Typography()
+
+// Back out the below once Typography is upgraded for es6
+export default typography
+
+export const rhythm = typography.rhythm
+export const scale = typography.scale
+export const options = typography.options
