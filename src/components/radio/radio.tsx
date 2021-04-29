b401a70deac257d5b74b2197f56cf60690edf8af
unknown <lcanavesio@gmail.com>
2021-04-29 01:41:27 -0300

    improvement radio component & getRadios url

--- src/components/radio/radio.tsx
+++ src/components/radio/radio.tsx
@@ -1,50 +1,108 @@
+import { makeStyles } from "@material-ui/core"
 import React, { useEffect, useState } from "react"
-import { RadioBrowserApi } from "radio-browser-api"
-import AudioPlayer from "react-h5-audio-player"
+import AudioPlayer, { RHAP_UI } from "react-h5-audio-player"
 import "react-h5-audio-player/lib/styles.css"
+import { getRadios, TRadios } from "../../utils/radiosConfig"
+
+const useStyles = makeStyles(() => ({
+  radio: {
+    maxWidth: "60em",
+    width: "100%;",
+  },
+  stations: {
+    display: "grid",
+    width: "100%",
+  },
+  stationName: {
+    display: "flex",
+    flexDirection: "row",
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "start",
+  },
+  img: {
+    display: "block",
+    width: "3.5em",
+    height: "3.5em",
+    borderRadius: "50%",
+    border: "2px solid rgb(32 28 37)",
+    margin: "0 0.25em",
+    "&:hover": {
+      borderColor: "green",
+    },
+  },
+  name: {
+    width: "100%",
+    textAlign: "center",
+    fontFamily: "Architects Daughter,  cursive",
+  },
+  station: {
+    fontSize: "1.2em",
+    border: "1px solid rgb(32 28 37)",
+    margin: "0.25em",
+    borderRadius: "10px",
+    backgroundColor: "#fff",
+    padding: "1em",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    "&:hover": {
+      borderColor: "green",
+    },
+  },
+}))
 
 const Radio = () => {
-  const [stations, setStations] = useState()
-  const [stationFilter, setStationFilter] = useState("all")
+  const classes = useStyles()
 
-  useEffect(() => {}, [stationFilter])
+  const [stations, setStations] = useState<TRadios[]>()
+  const [stationFilter, setStationFilter] = useState()
 
-  const setupApi = async stationFilter => {
-    const api = new RadioBrowserApi(fetch.bind(window), "My Radio App")
+  useEffect(() => {
+    const dataRadios = getRadios()
+    setStations(dataRadios)
+  }, [stationFilter])
 
-    const stations = []
-  }
-
-  const filters = [
-    "all",
-    "classical",
-    "country",
-    "dance",
-    "disco",
-    "house",
-    "jazz",
-    "pop",
-    "rap",
-    "retro",
-    "rock",
-  ]
+  const filters = ["fmPasion", "latina", "libertad", "universo"]
 
   const setDefaultSrc = event => {
     event.target.src = "/images/defaultRadio.png"
   }
 
   return (
-    <div className="radio">
-          <AudioPlayer
-            autoPlay
-            src="http://example.com/audio.mp3"
-            onPlay={e => console.log("onPlay")}
-
-            // other props here
-          />
-        </div>
-
-
+    <div className={classes.stations}>
+      {stations &&
+        stations.map((station, index) => {
+          return (
+            <div className={classes.station} key={index}>
+              <div className={classes.stationName}>
+                <img
+                  className={classes.img}
+                  src={station.imgLogo}
+                  alt="station logo"
+                  onError={setDefaultSrc}
+                />
+               
+                <AudioPlayer
+                  style={{  backgroundColor: "rgb(238 236 241)",
+                    display: "flex",
+                    justifyItems: "center",
+                    padding: "0.25em 0.75em",
+                    borderRadius: "10px" }}
+                  src={station.streamUrl}
+                  showJumpControls={false}
+                  layout="stacked"
+                  customProgressBarSection={[]}
+                  autoPlayAfterSrcChange={false}
+                  preload={"metadata"}
+                  customControlsSection={[RHAP_UI.MAIN_CONTROLS, RHAP_UI.VOLUME_CONTROLS]}
+                />
+              </div>
+            </div>
+          )
+        })}
+    </div>
   )
 }
 
