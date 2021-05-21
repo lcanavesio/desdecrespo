6408afd74cda60047ac38a6d3a42ba648bf6b49d
renzopercara <renzopercara@gmail.com>
2021-05-20 21:49:12 -0300

    Logic to generic search

--- src/components/post/postCard.tsx
+++ src/components/post/postCard.tsx
@@ -11,7 +11,6 @@ const useStyles = makeStyles((theme) => ({
         textDecoration: 'none',
     },
     card: {
-        maxWidth: 300,
         margin: "auto",
         transition: "0.3s",
         boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
