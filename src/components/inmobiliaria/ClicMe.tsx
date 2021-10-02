141445365f6105a6f37feea4d19f3d6147d82769
lcanavesio <lcanavesio@gmail.com>
2021-10-02 13:37:53 -0300

    fix: icons ClicMe

--- src/components/inmobiliaria/ClicMe.tsx
+++ src/components/inmobiliaria/ClicMe.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../css/clicme.css';
 import AdvancedImageList from './AdvancedImageList';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import LanguageIcon from '@material-ui/icons/Language';
 const ClicMe = () => {
   return (
     <>
@@ -19,10 +23,10 @@ const ClicMe = () => {
       </a>
       <AdvancedImageList key="advancedImagelist" />
       <div id="social-platforms">
-        <a className="btn btn-icon btn-facebook" href="https://www.desdecrespo.com.ar/wp-content/uploads/SmartLinks/crespo-inmobiliairia/crespo-inmobiliairia.html" target="_blank" rel="noreferrer"><i className="fa fa-facebook"></i><span>Ingresá al Facebook </span></a>
-        <a className="btn btn-icon btn-instagram" href="https://instagram.com/crespoinmobiliaria" target="_blank" rel="noreferrer"><i className="fa fa-instagram"></i><span>Ingresá al Instagram</span></a>
-        <a className="btn btn-icon btn-whatsapp" href="https://api.whatsapp.com/send?phone=5493435037590" target="_blank" rel="noreferrer"><i className="fa fa-whatsapp"></i><span>Escribinos al Whatsapp</span></a>
-        <a className="btn btn-icon btn-globe" href="https://www.crespoinmobiliaria.com.ar" target="_blank" rel="noreferrer"><i className="fa fa-globe"></i><span>Ingresá a la Web</span></a>
+        <a className="btn btn-icon btn-facebook" href="https://www.desdecrespo.com.ar/wp-content/uploads/SmartLinks/crespo-inmobiliairia/crespo-inmobiliairia.html" target="_blank" rel="noreferrer"><i className="fa fa-facebook"><FacebookIcon /></i><span>Ingresá al Facebook </span></a>
+        <a className="btn btn-icon btn-instagram" href="https://instagram.com/crespoinmobiliaria" target="_blank" rel="noreferrer"><i className="fa fa-instagram"><InstagramIcon /></i><span>Ingresá al Instagram</span></a>
+        <a className="btn btn-icon btn-whatsapp" href="https://api.whatsapp.com/send?phone=5493435037590" target="_blank" rel="noreferrer"><i className="fa fa-whatsapp"><WhatsAppIcon /> </i><span>Escribinos al Whatsapp</span></a>
+        <a className="btn btn-icon btn-globe" href="https://www.crespoinmobiliaria.com.ar" target="_blank" rel="noreferrer"><i className="fa fa-globe"><LanguageIcon/></i><span>Ingresá a la Web</span></a>
       </div>
 
 
